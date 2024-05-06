4145b97f1d72d5640f6e1ee7a5ad9b01fccbb3e8
Craig Stuntz <CraigStuntz@users.noreply.github.com>
2024-05-05 22:05:41 -0400

    More type checking/synthesis

--- Sources/Bidi/SimplyTypedLambda.swift
+++ Sources/Bidi/SimplyTypedLambda.swift
@@ -2,17 +2,29 @@ public typealias Name = String
 
 public enum Message: Error, CustomStringConvertible {
   case cannotType(Expr)
+  /// Associated values are the name of the function, the expected type, and the actual type
+  case incorrectType(Name, Type, Type)
+  case lambdaRequiresArrow(Type)
   case notAFunction(Type)
   case notANat(Type)
   case notFound(Name)
+  /// Associated values are the expected type and the actual type
+  case unexpectedType(Type, Type)
 
   public var description: String {
     switch self {
     case .cannotType(let other):
-      return "Cannot find a type for \(String(describing: other)). Try adding a type annotation."
+      return "Can't find a type for \(String(describing: other)). Try adding a type annotation."
+    case .incorrectType(let name, let expected, let other):
+      return
+        "\(name) should be a \(String(describing: expected)), but was used where a \(String(describing: other)) was expected"
+    case .lambdaRequiresArrow(let other):
+      return "Lambda requires a function type, but got \(String(describing: other))"
     case .notAFunction(let other): return "Not a function type: \(String(describing: other))"
     case .notANat(let other): return "Not the type Nat: \(String(describing: other))"
     case .notFound(let name): return "Not found: \(name)"
+    case .unexpectedType(let expected, let other):
+      return "Expected \(String(describing: expected)) but got \(String(describing: other))"
     }
   }
 }
@@ -27,27 +39,45 @@ public indirect enum Expr {
   case zero
   /// Another constructor of tnat
   case add1(Expr)
-  /// Another tarr eliminator
+  /// recursion is primitive recursion on Nat. The associated values are the type of the
+  /// result, target, base, and step. If target is zero, then the whole
+  /// expression is base. If target is (add1 n), then the whole expression is
+  /// (step n (rec-Nat n base step)). Another tarr eliminator.
   case recursion(Type, Expr, Expr, Expr)
   case annotation(Expr, Type)
 }
 
-public indirect enum Type {
+public indirect enum Type: Equatable {
   /// The type of natural numbers
   case tnat
-  /// Arrow type (function)
+  /// Arrow type (function), Associated values are arg and ret (the type of the
+  /// argument to the function and the type the function returns)
   case tarr(Type, Type)
 }
 
 public struct Context {
   let values: [Name: Type]
 
+  public init(values: [Name: Type]) {
+    self.values = values
+  }
+
   public init() {
-    self.values = [:]
+    self.init(values: [:])
+  }
+
+  func extend(name: Name, type: Type) -> Context {
+    var result = values
+    result[name] = type
+    return Context(values: result)
+  }
+
+  subscript(name: Name) -> Type? {
+    return self.values[name]
   }
 
   public func lookup(name: Name) -> Result<Type, Message> {
-    guard let type = values[name] else {
+    guard let type = self[name] else {
       return .failure(.notFound(name))
     }
     return .success(type)
@@ -61,29 +91,56 @@ public struct Context {
         ty in
         switch ty {
         case .tarr(let argT, let retT):
-          return check(expr: rand, ty: argT).flatMap { () in .success(retT) }
-        default: return .failure(.notAFunction(ty))
+          return check(expr: rand, type: argT).flatMap { () in .success(retT) }
+        case .tnat: return .failure(.notAFunction(ty))
         }
       }
     case .recursion(let ty, let tgt, let base, let step):
       return self.synth(expr: tgt).flatMap { tgtT in
         switch tgtT {
         case .tnat:
-          return check(expr: base, ty: tgtT).flatMap {
+          return check(expr: base, type: tgtT).flatMap {
             () in
-            check(expr: step, ty: .tarr(.tnat, .tarr(ty, ty))).flatMap {
+            check(expr: step, type: .tarr(.tnat, .tarr(ty, ty))).flatMap {
               () in .success(ty)
             }
           }
-        default: return .failure(.notANat(tgtT))
+        case .tarr: return .failure(.notANat(tgtT))
         }
       }
-    case .annotation(let e, let t): return check(expr: e, ty: t).flatMap { () in .success(t) }
-    default: return .failure(.cannotType(expr))
+    case .annotation(let e, let t): return check(expr: e, type: t).flatMap { () in .success(t) }
+    case .add1, .lambda, .zero: return .failure(.cannotType(expr))
     }
   }
 
-  public func check(expr: Expr, ty: Type) -> Result<Void, Message> {
-    return .failure(.cannotType(expr))
+  public func check(expr: Expr, type: Type) -> Result<Void, Message> {
+    switch expr {
+    case .lambda(let x, let body):
+      switch type {
+      case .tarr(let arg, let ret):
+        return
+          self
+          .extend(name: x, type: arg)
+          .check(expr: body, type: ret)
+      case .tnat: return .failure(.lambdaRequiresArrow(type))
+      }
+    case .zero:
+      switch type {
+      case .tnat: return .success(())
+      case .tarr: return .failure(.incorrectType("Zero", .tnat, type))
+      }
+    case .add1(let n):
+      switch type {
+      case .tnat: return self.check(expr: n, type: .tnat)
+      case .tarr: return .failure(.incorrectType("Add1", .tnat, type))
+      }
+    default:
+      return synth(expr: expr).flatMap { type2 in
+        if type == type2 {
+          return .success(())
+        }
+        return .failure(.unexpectedType(type, type2))
+      }
+    }
   }
 }
