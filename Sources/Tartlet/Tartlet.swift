4e30ce95458bc3e528c1ed9bc8c79007641f04d9
Craig Stuntz <CraigStuntz@users.noreply.github.com>
2024-06-01 22:32:16 -0400

    Improve docstrings

--- Sources/Tartlet/Tartlet.swift
+++ Sources/Tartlet/Tartlet.swift
@@ -2,41 +2,41 @@ import Shared
 
 public indirect enum Expr {
   case variable(Name)
-  /// Associated type names are x, A, and B as in (Π ((x A)) B)
+  /// Associated type names are x, A, and B as in `(Π ((x A)) B)`
   case pi(Name, Expr, Expr)
-  /// Associated type names are x (arg name) and b(ody) as in (λ (x) b)
+  /// Associated type names are x (arg name) and b(ody) as in `(λ (x) b)`
   case lambda(Name, Expr)
-  /// Associated type names are (ope)rator and (ope)rand as in (rator rand)
+  /// Associated type names are (ope)rator and (ope)rand as in `(rator rand)`
   case application(Expr, Expr)
-  /// Associated type names are x A and D as in (Σ ((x A)) D)
+  /// Associated type names are x A and D as in `(Σ ((x A)) D)`
   case sigma(Name, Expr, Expr)
-  /// Associated type names are as in (cons a d)
+  /// Associated type names are as in `(cons a d)`
   case cons(Expr, Expr)
-  /// Associated type is expression as in (car e)
+  /// Associated type is expression as in `(car e)`
   case car(Expr)
-  /// Associated type is expression as in (cdr c)
+  /// Associated type is expression as in `(cdr c)`
   case cdr(Expr)
   case nat
   case zero
-  /// Associated type as in (add1 e)
+  /// Associated type as in `(add1 e)`
   case add1(Expr)
-  /// Associated types as in (ind-Nat tgt mot base step)
+  /// Associated types as in `(ind-Nat tgt mot base step)`
   case indnat(Expr, Expr, Expr, Expr)
-  /// Associated types as in (= A from t)
+  /// Associated types as in `(= A from t)`
   case equal(Expr, Expr, Expr)
   case same
-  /// Associated types as in (replace tgt mot base)
+  /// Associated types as in `(replace tgt mot base)`
   case replace(Expr, Expr, Expr)
   case trivial
   case sole
   case absurd
-  /// Associated types as in (ind-Absurd tgt mot)
+  /// Associated types as in `(ind-Absurd tgt mot)`
   case indabsurd(Expr, Expr)
   case atom
-  /// 'a'
+  /// A sumbol, with associated type of the name, as in `'a`
   case tick(String)
   case u
-  /// Associated types as in (the t e)
+  /// Associated types as in `(the t e)`
   case the(Expr, Expr)
 
   public func αEquiv(_ other: Expr) -> Bool {
